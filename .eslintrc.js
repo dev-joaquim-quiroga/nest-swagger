c277452166e73dc8c297b67b2bc7107a8ca92a2f
dev-joaquim-quiroga <61553285+dev-joaquim-quiroga@users.noreply.github.com>
2020-05-16 22:01:55 -0400

    Feature/add swagger (#1)
    * add swagger
    
    * add swagger

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   env: {
     node: true,
     jest: true,
+    es6: true
   },
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
---
+++ src/cats/cat.ts
@@ -0,0 +1,8 @@
+import {  ApiProperty } from '@nestjs/swagger';
+
+export class Cat {
+  @ApiProperty()
+  id: number;
+  @ApiProperty()
+  name: string;
+}
